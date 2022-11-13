666c8edeba5be879a92a5edc2cbdda5f835cecc7
gmcln-rct <producersama@gmail.com>
2022-11-13 11:36:09 -0500

    Add dynamic head content

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
@@ -76,6 +77,13 @@ function FilteredEventsPage(props) {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <Fragment>
+        <Head>
+          <title>Filtered Events</title>
+          <meta 
+            name='description'
+            content={`All events for ${numMonth}/${numYear}.`}
+          />
+        </Head>
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
@@ -90,6 +98,13 @@ function FilteredEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Filtered Events</title>
+        <meta
+          name='description'
+          content={`All events for ${numMonth}/${numYear}.`}
+        />
+      </Head>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,4 @@
-
+import Head from 'next/head';
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
@@ -18,6 +18,13 @@ function AllEventsPage(props) {
 
   return (
     <Fragment>
+        <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
